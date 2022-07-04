8bde0e1c87e36ae9396f516162656204cac513cc
Namanh Kapur <namanhkapur1205@gmail.com>
2022-07-04 17:43:06 -0500

    changed

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ def func_b(id: int):
     # do some things
     return id + 55
 
-def func_a(obj: O):
+def func_a(id: int):
     # do some more things
-    b = func_b(obj)
+    b = func_b(id)
     return b
